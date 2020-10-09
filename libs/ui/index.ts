5ef47307727caf7191b54cedcdbc0f7b9984d93a
Chiru B <33750251+itsezc@users.noreply.github.com>
2020-10-09 05:33:51 +0530

    [Components] Button and Avatar

---
+++ libs/ui/index.ts
@@ -0,0 +1,2 @@
+export { Avatar } from './source/app/avatar/avatar'
+export { Button } from './source/app/button/button'
---
+++ libs/ui/source/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Avatar } from './avatar/avatar';
+
+export const App = () => {
+	return (
+		<Avatar 
+			src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/michele-morrone-attends-the-bar-giuseppe-red-carpet-during-news-photo-1592521999.jpg?crop=1.00xw:0.667xh;0,0.0658xh&resize=480:*'
+			name='Chiru Boggavarapu'
+		/>
+  	);
+};
+
+export default App;
